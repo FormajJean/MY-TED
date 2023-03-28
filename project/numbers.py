f0af013b48de1581f4abd5c1c38c0cfb96023fbd
Азамат <xismatullin2003@inbox.ru>
2023-03-28 15:23:38 +0300

    TIKET-001: add function plus

--- project/numbers.py
+++ project/numbers.py
@@ -5,8 +5,11 @@ texts = 'fdfd'
 def text(text):
     print(text)
 
+def plus(a, b):
+    return a + b
+
 if __name__ == '__main__':
     print(main(1, 24))
     text(texts)
-
+    plus(15, 243)
 print('JIMI NEITRON')
